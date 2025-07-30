fe3151a4bc98cc1b523c1e5889ba2467b0d75e12
Yena Park <yenapark@nyu.edu>
2025-07-30 16:07:18 -0400

    refactored calculator

--- py101/lesson_2/calculator.py
+++ py101/lesson_2/calculator.py
@@ -1,16 +1,43 @@
-print('Welcome to Calculator!')
-num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
-operation = input('What operation would you like to perform?\n'
-                  '1) Add 2) Subtract 3) Multiply 4) Divide ')
+def prompt(message):
+    print(f'==> {message}')
 
-if operation == '1':
-    output = num1 + num2
-elif operation == '2':
-    output = num1 - num2
-elif operation == '3':
-    output = num1 * num2
-elif operation == '4':
-    output = num1 / num2
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+    
+    return False
 
-print(f'The result is: {output}')
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number? ")
+num1 = input()
+while invalid_number(num1):
+    prompt('Please enter a valid number.')
+    num1 = input()
+
+prompt("What's the second number? ")
+num2 = input()
+while invalid_number(num2):
+    prompt('Please enter a valid number.')
+    num2 = input()
+
+prompt('What operation would you like to perform?\n'
+       '1) Add 2) Subtract 3) Multiply 4) Divide ')
+operation = input()
+while operation not in ['1', '2', '3', '4']:
+    prompt('Please choose 1, 2, 3, or 4.')
+    operation = input()
+
+match operation:
+    case '1':
+        output = int(num1) + int(num2)
+    case '2':
+        output = int(num1) - int(num2)
+    case '3':
+        output = int(num1) * int(num2)
+    case '4':
+        output = int(num1) / int(num2)
+
+prompt(f'The result is: {output}')
