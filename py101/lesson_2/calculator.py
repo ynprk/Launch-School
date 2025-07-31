d2a67e6f57bbdc39d2c4dcb25829d0c3b84ffe04
Yena Park <yenapark@nyu.edu>
2025-07-31 15:48:42 -0400

    calculator bonus feature 2

--- py101/lesson_2/calculator.py
+++ py101/lesson_2/calculator.py
@@ -1,3 +1,9 @@
+import json
+
+# open the JSON file for reading
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 def prompt(message):
     print(f'==> {message}')
 
@@ -9,23 +15,22 @@ def invalid_number(number_str):
     return False
 
 def perform_calculation():
-    prompt("What's the first number? ")
+    prompt(MESSAGES['first_number'])
     num1 = input()
     while invalid_number(num1):
-        prompt('Please enter a valid number.')
+        prompt(MESSAGES['invalid_number'])
         num1 = input()
 
-    prompt("What's the second number? ")
+    prompt(MESSAGES['second_number'])
     num2 = input()
     while invalid_number(num2):
-        prompt('Please enter a valid number.')
+        prompt(MESSAGES['invalid_number'])
         num2 = input()
 
-    prompt('What operation would you like to perform?\n'
-        '1) Add 2) Subtract 3) Multiply 4) Divide ')
+    prompt(MESSAGES['which_operation'])
     operation = input()
     while operation not in ['1', '2', '3', '4']:
-        prompt('Please choose 1, 2, 3, or 4.')
+        prompt(MESSAGES['invalid_operation'])
         operation = input()
 
     match operation:
@@ -38,14 +43,15 @@ def perform_calculation():
         case '4':
             output = int(num1) / int(num2)
 
-    prompt(f'The result is: {output}')
+    prompt(f'{MESSAGES["result"]} {output}')
 
-prompt('Welcome to Calculator!')
+# main program
+prompt(MESSAGES['welcome'])
 again = 'y'
 while again == 'y':
     perform_calculation()
-    prompt(f'Would you like to perform a new calculation? (y/n)')
+    prompt(MESSAGES['new_calculation'])
     again = input().lower()
     while again not in ['y', 'n']:
-        prompt('Please type y or n')
+        prompt(MESSAGES['invalid_new'])
         again = input().lower()
