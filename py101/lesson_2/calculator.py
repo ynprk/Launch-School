48a9bcca48fd75ed7aa741c55060beefb3d45da7
Yena Park <yenapark@nyu.edu>
2025-07-31 15:21:56 -0400

    calculator bonus feature 1

--- py101/lesson_2/calculator.py
+++ py101/lesson_2/calculator.py
@@ -8,35 +8,44 @@ def invalid_number(number_str):
         return True
     return False
 
-prompt('Welcome to Calculator!')
-
-prompt("What's the first number? ")
-num1 = input()
-while invalid_number(num1):
-    prompt('Please enter a valid number.')
+def perform_calculation():
+    prompt("What's the first number? ")
     num1 = input()
+    while invalid_number(num1):
+        prompt('Please enter a valid number.')
+        num1 = input()
 
-prompt("What's the second number? ")
-num2 = input()
-while invalid_number(num2):
-    prompt('Please enter a valid number.')
+    prompt("What's the second number? ")
     num2 = input()
+    while invalid_number(num2):
+        prompt('Please enter a valid number.')
+        num2 = input()
 
-prompt('What operation would you like to perform?\n'
-       '1) Add 2) Subtract 3) Multiply 4) Divide ')
-operation = input()
-while operation not in ['1', '2', '3', '4']:
-    prompt('Please choose 1, 2, 3, or 4.')
+    prompt('What operation would you like to perform?\n'
+        '1) Add 2) Subtract 3) Multiply 4) Divide ')
     operation = input()
+    while operation not in ['1', '2', '3', '4']:
+        prompt('Please choose 1, 2, 3, or 4.')
+        operation = input()
 
-match operation:
-    case '1':
-        output = int(num1) + int(num2)
-    case '2':
-        output = int(num1) - int(num2)
-    case '3':
-        output = int(num1) * int(num2)
-    case '4':
-        output = int(num1) / int(num2)
+    match operation:
+        case '1':
+            output = int(num1) + int(num2)
+        case '2':
+            output = int(num1) - int(num2)
+        case '3':
+            output = int(num1) * int(num2)
+        case '4':
+            output = int(num1) / int(num2)
 
-prompt(f'The result is: {output}')
+    prompt(f'The result is: {output}')
+
+prompt('Welcome to Calculator!')
+again = 'y'
+while again == 'y':
+    perform_calculation()
+    prompt(f'Would you like to perform a new calculation? (y/n)')
+    again = input().lower()
+    while again not in ['y', 'n']:
+        prompt('Please type y or n')
+        again = input().lower()
