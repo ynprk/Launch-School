1bd5d757d4b37f9d6d47e1145d3d07bee9a594d9
Yena Park <yenapark@nyu.edu>
2025-09-10 18:57:06 -0400

    ttt refactored

--- py110/lesson_3/ttt.py
+++ py110/lesson_3/ttt.py
@@ -1,109 +1,66 @@
 import os, random
 
-INITIAL_MARKER = ' '
-HUMAN_MARKER = 'X'
-COMPUTER_MARKER = 'O'
-
-def initialize_board():
-    return {square: INITIAL_MARKER for square in range(1, 10)}
+HUMAN, COMPUTER = 'X', 'O'
+EMPTY = None
+WINNING_LINES = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  # cols
+    [0, 4, 8], [2, 4, 6]              # diags
+]
 
 def display_board(board):
     os.system('clear')
+    symbols = [(c if c else " ") for c in board]
 
-    print('')
-    print('     |     |')
-    print(f'  {board[1]}  |  {board[2]}  |  {board[3]}')
-    print('     |     |')
-    print('-----+-----+-----')
-    print('     |     |')
-    print(f'  {board[4]}  |  {board[5]}  |  {board[6]}')
-    print('     |     |')
-    print('-----+-----+-----')
-    print('     |     |')
-    print(f'  {board[7]}  |  {board[8]}  |  {board[9]}')
-    print('     |     |')
-    print('')
+    for r in range(0, 9, 3):
+        print('     |     |')
+        print(f'  {symbols[r]}  |  {symbols[r+1]}  |  {symbols[r+2]}')
+        print('     |     |')
+        if r < 6:
+            print('-----+-----+-----')
+        print()
 
 def empty_squares(board):
-    return [key for key, value in board.items() if value == INITIAL_MARKER]
+    return [i for i, c in enumerate(board) if c is EMPTY]
 
-def player_turn(board):
-    while True:
-        valid_choices = [str(num) for num in empty_squares(board)]
-        square = input(f'Choose a square ({", ".join(valid_choices)}): ')
-
-        if square.strip().isdigit() and int(square.strip()) in empty_squares(board):
-            square = int(square.strip())
-            break
-
-        print("Sorry, that's not a valid choice.")
-
-    board[int(square)] = HUMAN_MARKER
-
-def computer_turn(board):
-    square = random.choice(empty_squares(board))
-    board[square] = COMPUTER_MARKER
-
-def detect_winner(board):
-    winning_lines = [
-        [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
-        [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
-        [1, 5, 9], [3, 5, 7]             # diagonials
-    ]
-
-    for line in winning_lines:
-        sq1, sq2, sq3 = line
-        if (board[sq1] == HUMAN_MARKER
-                and board[sq2] == HUMAN_MARKER
-                and board[sq3] == HUMAN_MARKER):
-            return 'Player'
-        elif (board[sq1] == COMPUTER_MARKER
-                and board[sq2] == COMPUTER_MARKER
-                and board[sq3] == COMPUTER_MARKER):
-            return 'Computer'
-        
+def determine_winner(board):
+    for a, b, c in WINNING_LINES:
+        if board[a] and board[a] == board[b] == board[c]:
+            return HUMAN if board[a] == HUMAN else COMPUTER
     return None
 
+def full(board):
+    return all(board)
 
-def board_full(board) -> bool:
-    return len(empty_squares(board)) == 0
+def ask_move(board):
+    valid_choices = [str(i+1) for i in empty_squares(board)]
 
-def set_flag():
     while True:
-        keep_going = input('Would you like to play again? (y/n): ')
-        if keep_going.strip().lower() in ('y', 'n'):
-            break
-        print('Please enter y or n')
-    return keep_going
+        move = input(f'Choose a square ({", ".join(valid_choices)}): ').strip()
+        if move in valid_choices:
+            return int(move) - 1
+        print("Sorry, that's not a valid choice.")
+
+def play():
+    board = [EMPTY] * 9
+    while True:
+        display_board(board)
+
+        board[ask_move(board)] = HUMAN
+        if (winner := determine_winner(board)) or full(board): break
+
+        board[random.choice(empty_squares(board))] = COMPUTER
+        if (winner := determine_winner(board)) or full(board): break
+
+    display_board(board)
+    print(f'{winner} wins!' if winner else "It's a tie!")
 
 def main():
     while True:
-        print('Welcome to Tic Tac Toe')
-        print(f'You are {HUMAN_MARKER}. Computer is {COMPUTER_MARKER}.')
-
-        board = initialize_board()
-
-        while True:
-            display_board(board)
-
-            player_turn(board)
-            if detect_winner(board) or board_full(board):
-                display_board(board)
-                break
-
-            computer_turn(board)
-            if detect_winner(board) or board_full(board):
-                display_board(board)
-                break
-            
-        if detect_winner(board):
-            print(f'{detect_winner(board)} won!')
-        else:
-            print("It's a tie!")
-
-
-        if set_flag() == 'n':
-            print('Bye Bye')
+        print(f'Welcome to Tic Tac Toe! You = {HUMAN}, Computer = {COMPUTER}')
+        play()
+        if input('Play again? (y/n) : ').strip().lower() != 'y':
+            print('Bye-bye')
             break
 
 if __name__ == '__main__':
